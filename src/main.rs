85f0b9be91b03f68a893bb0d0cb6c3cbfd759747
alejandrogzi <jose.gonzalesdezavala1@unmsm.edu.pe>
2023-08-25 01:10:52 -0500

    feat(args): support -cpu; default max_cpus

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use gtfsort::gtfsort;
+use gtfsort::{gtfsort, benchmark};
 
 use clap::{Arg, Command, ArgMatches};
 
 use colored::Colorize;
 
-use log::Level;
+use num_cpus;
 
 use std::string::String;
 use std::error::Error;
@@ -25,6 +25,12 @@ fn main() {
             .required(true)
             .value_name("OUTPUT")
             .help("Output gtf file"))
+        .arg(Arg::new("cpu")
+            .index(3)
+            .required(false)
+            .default_value("max")
+            .value_name("NUM_CPUS")
+            .help("Number of cpus to use"))
         .get_matches();
 
     if let Some(err) = run(matches).err() {
@@ -39,8 +45,37 @@ fn main() {
 fn run(matches: ArgMatches) -> Result<(), Box<dyn Error>> {
     let i: &String = matches.get_one("i").unwrap();
     let o: &String = matches.get_one("o").unwrap();
+    let cpu: &String = matches.get_one("cpu").unwrap();
+    let max = num_cpus::get();
 
-    let _ = gtfsort(i, o);
+    let n = match cpu.as_str() {
+        "max" => {
+            max
+        }
+        _ => {
+            if cpu.parse::<usize>().is_err() {
+                eprintln!("{} {}",
+                        "Error:".bright_red().bold(),
+                        "Number of cpus must be an integer".bright_red());
+                std::process::exit(1);
+            } else {
+                if cpu.parse::<usize>().unwrap() > max {
+                    eprintln!("{} {} {} {} {}",
+                            "Error:".bright_red().bold(),
+                            "Number of cpus must be less than or equal to".bright_red(),
+                            max.to_string().bright_red(),
+                            "\n Gtfsort will use:".bright_red().bold(),
+                            max.to_string().bright_red());
+                    max
+                } else {
+                    cpu.parse::<usize>().unwrap()
+                }
+            }
+        }
+    };
+
+    let _ = gtfsort(i, o, n);
+    // benchmark();
 
     Ok(())
 }
